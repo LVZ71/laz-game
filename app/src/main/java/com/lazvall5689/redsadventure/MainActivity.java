e25758374d7f7b35fd11433af1e2883d458d9249
oscar0812 <oscar0812torres@gmail.com>
2017-10-21 21:14:42 -0500

    button click

--- app/src/main/java/com/lazvall5689/redsadventure/MainActivity.java
+++ app/src/main/java/com/lazvall5689/redsadventure/MainActivity.java
@@ -1,13 +1,27 @@
 package com.lazvall5689.redsadventure;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context mainContext = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mainContext, "HERE", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
