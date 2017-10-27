ae7c31ee57def6305067ab47d676bbbfa774fbe6
oscar0812 <oscar0812torres@gmail.com>
2017-10-27 16:29:53 -0500

    Scene change logic done

--- app/src/main/java/com/lazvall5689/redsadventure/MainActivity.java
+++ app/src/main/java/com/lazvall5689/redsadventure/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lazvall5689.redsadventure;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,7 +17,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "redsadventure";   // helpful for debugging in Log
+    final Button[] buttons = new Button[4];
     private ArrayList<Scene> scenes = new ArrayList<>();   // will hold the scenes from the webpage
+    public int currentScene = 0; // will hold the current index in the array
+    TextView mainTextView;
 
     // first method called by android when app first starts
     @Override
@@ -24,43 +28,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button[] buttons = new Button[4];
+        mainTextView = (TextView)findViewById(R.id.textView);
         setButtonListeners(buttons);
+        setFontGlobally();
         // grab scenes from webpage
         grabScenesFromWeb();
     }
 
     // set the button image when held down and when released
-    private void setButtonListeners(Button[] buttons){
+    private void setButtonListeners(final Button[] buttons){
         for (int i = 1; i <= 4; i++) {
+            // button1 button2 button3 button4
             int id = getResources().getIdentifier("button" + i, "id", getPackageName());
             buttons[i - 1] = (Button) findViewById(id);
         }
 
-        for (final Button b : buttons)
-            b.setOnTouchListener(new View.OnTouchListener() {
+        // for every button do something
+        for (final Button btn : buttons) {
+            btn.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
                     switch (event.getAction()) {
                         case MotionEvent.ACTION_DOWN:
-                            b.setBackgroundResource(R.drawable.button_pressed);
+                            btn.setBackgroundResource(R.drawable.button_pressed);
                             break;
                         case MotionEvent.ACTION_UP:
-                            b.setBackgroundResource(R.drawable.button);
+                            btn.setBackgroundResource(R.drawable.button);
                             break;
                     }
                     return false;
                 }
             });
+
+            // get the button clicked and do some logic to change the scene
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String name = getResources().getResourceEntryName(btn.getId());
+                    int clicked = StringUtil.parseInt(name.charAt(name.length()-1)+"")-1;
+                    Scene current = scenes.get(currentScene);
+                    currentScene = current.getRoutes()[clicked];
+                    changeScenes(currentScene);
+
+                }
+            });
+        }
     }
 
     // call this after scenes are retrieved from webpage
     public void start(){
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(scenes.get(2).getContent());
-        printArr(scenes);
+        changeScenes(0);
+        //printArr(scenes);
     }
 
+    // grey out unused buttons
+    private void enableButtons(int num){
+        int counter = 0;
+        for(Button btn: buttons){
+            if(counter<num){
+                btn.setEnabled(true);
+                counter++;
+            }
+            else{
+                btn.setEnabled(false);
+            }
+        }
+    }
+
+    private void setFontGlobally(){
+        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/font.ttf");
+        mainTextView.setTypeface(type);
+        for(Button b: buttons){
+            b.setTypeface(type);
+        }
+    }
+    // change scene to scenes( numberInArr )
+    private void changeScenes(int numberInArr){
+        Scene currentScene = scenes.get(numberInArr);
+        mainTextView.setText(currentScene.getContent());
+        for(int x = 0; x<currentScene.numRoutes();x++){
+            Scene path = scenes.get(currentScene.getRoutes()[x]);
+            buttons[x].setText(path.getTitle());
+        }
+        enableButtons(currentScene.numRoutes());
+    }
+
+    // print an array of objects
     private void printArr(ArrayList list) {
         for (Object a : list) {
             Log.i(TAG, a.toString());
@@ -74,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
             public void scenesRetrieved(ArrayList<Scene> list) {
                 // set the scenes arrayList to the new one made in GetHtml
                 scenes = list;
-
                 // all set up, call start
                 start();
             }
